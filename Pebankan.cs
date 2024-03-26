35196d5fda66b5cdcb5622eb59e52bc25d9236ff
feby_ <feby_@DESKTOP-57AL339>
2024-03-26 20:30:29 +0700

    Add project files.

---
+++ Pebankan.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Nasabah
+{
+    public string Nama { get; set; }
+    public long  NomorRekening { get; set; }
+    public string Alamat { get; set; }
+
+    public Nasabah(string nama, long nomorRekening, string alamat)
+    {
+        Nama = nama;
+        NomorRekening = nomorRekening;
+        Alamat = alamat;
+    }
+}
+
+public interface IRekeningBank
+{
+    void SetorTunai(decimal jumlah);
+    void TarikTunai(decimal jumlah);
+    decimal CekSaldo();
+    void BungaBulanan();
+}
+
+
+public abstract class Rekening : IRekeningBank
+{
+    protected decimal saldo;
+    protected Nasabah nasabah;
+    protected decimal minimumSetoran = 50000;
+
+    public virtual void SetorTunai(decimal jumlah)
+    {
+        if (jumlah < minimumSetoran)
+        {
+            Console.WriteLine("Setoran Tunai minimal adalah Rp. 50.0000.");
+            return;
+        }
+        saldo += jumlah;
+        Console.WriteLine("Setor Tunai berhasil.");
+    }
+
+    public virtual void TarikTunai(decimal jumlah)
+    {
+        saldo -= jumlah;
+    }
+
+    public decimal CekSaldo()
+    {
+        return saldo;
+    }
+
+    public Nasabah GetNasabah()
+    {
+        return nasabah;
+    }
+
+    public void SetNasabah(Nasabah nasabah)
+    {
+        this.nasabah = nasabah;
+    }
+
+    public abstract void BungaBulanan();
+}
+
+public class Tabungan : Rekening
+{
+    private decimal bunga;
+
+    public Tabungan(decimal saldoAwal, decimal bunga, Nasabah nasabah)
+    {
+        this.saldo = saldoAwal;
+        this.bunga = bunga;
+        SetNasabah(nasabah);
+    }
+
+    public override void SetorTunai(decimal jumlah)
+    {
+        base.SetorTunai(jumlah);
+    }
+
+    public void TambahBunga()
+    {
+        saldo += saldo * bunga;
+    }
+
+    public override void BungaBulanan()
+    {
+        saldo += saldo * bunga;
+    }
+}
+
+public class Giro : Rekening
+{
+    private decimal LimitPenarikan;
+
+    public Giro(decimal saldoAwal, decimal batasPenarikan, Nasabah nasabah)
+    {
+        this.saldo = saldoAwal;
+        this.LimitPenarikan = batasPenarikan;
+        SetNasabah(nasabah);
+    }
+
+    public override void TarikTunai(decimal jumlah)
+    {
+        if (saldo - jumlah >= -LimitPenarikan)
+        {
+            saldo -= jumlah;
+        }
+        else
+        {
+            Console.WriteLine("Penarikan melebihi saldo dan batas penarikan.");
+        }
+    }
+
+    public override void BungaBulanan()
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perbankan
+{
+   
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Nasabah nasabah = new Nasabah("cha cha maricha", 45690752783813, "Jl. Sumatra No. 76");
+
+                Tabungan tabungan = new Tabungan(50000, 0.05m, nasabah);
+                Giro giro = new Giro(50000, 200000, nasabah);
+
+                bool exit = false;
+                while (!exit)
+                {
+                    Console.WriteLine("Menu:");
+                    Console.WriteLine("1. Setoran");
+                    Console.WriteLine("2. Penarikan");
+                    Console.WriteLine("3. Cek Saldo");
+                    Console.WriteLine("4. Keluar");
+                    Console.Write("Pilih menu (1-4): ");
+
+                    int pilihan;
+                    if (int.TryParse(Console.ReadLine(), out pilihan))
+                    {
+                        switch (pilihan)
+                        {
+                            case 1:
+                                Console.WriteLine("Daftar Rekening:");
+                                Console.WriteLine("1. Rekening Tabungan");
+                                Console.WriteLine("2. Rekening Giro");
+                                Console.Write("Pilih nomor rekening untuk setoran: ");
+                                int nomorRekeningSetoran;
+                                if (int.TryParse(Console.ReadLine(), out nomorRekeningSetoran))
+                                {
+                                    Console.Write("Masukkan jumlah setoran: ");
+                                    decimal jumlahSetoran;
+                                    if (decimal.TryParse(Console.ReadLine(), out jumlahSetoran))
+                                    {
+                                        if (nomorRekeningSetoran == 1)
+                                        {
+                                            tabungan.SetorTunai(jumlahSetoran);
+
+                                        }
+                                        else if (nomorRekeningSetoran == 2)
+                                        {
+                                            giro.SetorTunai(jumlahSetoran);
+
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Nomor rekening tidak valid.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Masukan tidak valid.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nomor rekening tidak valid.");
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("Daftar Rekening:");
+                                Console.WriteLine("1. Rekening Tabungan");
+                                Console.WriteLine("2. Rekening Giro");
+                                Console.Write("Pilih nomor rekening untuk penarikan: ");
+                                int nomorRekeningPenarikan;
+                                if (int.TryParse(Console.ReadLine(), out nomorRekeningPenarikan))
+                                {
+                                    Console.Write("Masukkan jumlah penarikan: ");
+                                    decimal jumlahPenarikan;
+                                    if (decimal.TryParse(Console.ReadLine(), out jumlahPenarikan))
+                                    {
+                                        if (nomorRekeningPenarikan == 1)
+                                        {
+                                            tabungan.TarikTunai(jumlahPenarikan);
+                                            Console.WriteLine("Penarikan berhasil dari Rekening Tabungan.");
+                                        }
+                                        else if (nomorRekeningPenarikan == 2)
+                                        {
+                                            giro.TarikTunai(jumlahPenarikan);
+                                            Console.WriteLine("Penarikan berhasil dari Rekening Giro.");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Nomor rekening tidak valid.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Masukan tidak valid.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nomor rekening tidak valid.");
+                                }
+                                break;
+                            case 3:
+                                Console.WriteLine("Daftar Rekening:");
+                                Console.WriteLine("1. Rekening Tabungan");
+                                Console.WriteLine("2. Rekening Giro");
+                                Console.Write("Pilih nomor rekening untuk cek saldo: ");
+                                int nomorRekeningCekSaldo;
+                                if (int.TryParse(Console.ReadLine(), out nomorRekeningCekSaldo))
+                                {
+                                    decimal saldo = 0;
+                                    if (nomorRekeningCekSaldo == 1)
+                                    {
+                                        saldo = tabungan.CekSaldo();
+                                    }
+                                    else if (nomorRekeningCekSaldo == 2)
+                                    {
+                                        saldo = giro.CekSaldo();
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Nomor rekening tidak valid.");
+                                    }
+
+                                    if (saldo != 0)
+                                    {
+                                        Console.WriteLine($"Saldo Rekening {nomorRekeningCekSaldo}: {saldo}");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nomor rekening tidak valid.");
+                                }
+                                break;
+                            case 4:
+                                exit = true;
+                                break;
+                            default:
+                                Console.WriteLine("Menu tidak valid.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Masukan tidak valid.");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    
+}
